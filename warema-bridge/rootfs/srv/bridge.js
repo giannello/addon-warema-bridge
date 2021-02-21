e7c147c7f57e938698569e477d1b77d9d4cd885d
Giuseppe Iannello <giuseppe.iannello@brokenloop.net>
2021-02-21 18:31:45 +0100

    Cleanup

--- warema-bridge/rootfs/srv/bridge.js
+++ warema-bridge/rootfs/srv/bridge.js
@@ -1,21 +1,13 @@
-const WmsVbStickUsb = require('warema-wms-venetian-blinds');
-// const fs = require('fs')
-// const lineReader = require('line-reader');
+const warema = require('warema-wms-venetian-blinds');
+var mqtt = require('mqtt')
 
-const path = '/data/devices'
 
 const settingsPar = {
     wmsChannel   : process.env.WMS_CHANNEL     || 17,
     wmsKey       : process.env.WMS_KEY         || '00112233445566778899AABBCCDDEEFF',
     wmsPanid     : process.env.WMS_PAN_ID      || 'FFFF',
     wmsSerialPort: process.env.WMS_SERIAL_PORT || '/dev/ttyUSB0',
-    wsPort       : process.env.WMS_WS_PORT     || 8080,
-    wsPath       : process.env.WMS_WS_PATH     || '/'
   };
-
-// var wsApi = new WmsVbWsApi(settingsPar);
-var mqtt = require('mqtt')
-var client = mqtt.connect(process.env.MQTT_SERVER, {username: process.env.MQTT_USER, password: process.env.MQTT_PASSWORD})
 
 function callback(err, msg) {
   if(err) {
@@ -24,6 +16,7 @@ function callback(err, msg) {
   if(msg) {
     switch (msg.topic) {
       case 'wms-vb-init-completion':
+        console.log('Warema init completed')
         stickUsb.setPosUpdInterval(5000);
         stickUsb.scanDevices({autoAssignBlinds: false});
         break
@@ -45,8 +38,6 @@ function callback(err, msg) {
             position_closed: 100,
             unique_id: element.snr
           }
-          console.log('TOPIC: ' + topic);
-          console.log('PAYLOAD: ' + JSON.stringify(payload));
           client.publish(topic, JSON.stringify(payload))
         });
         console.log(stickUsb.vnBlindsList())
@@ -57,20 +48,18 @@ function callback(err, msg) {
   }
 }
 
+var client = mqtt.connect(process.env.MQTT_SERVER, {username: process.env.MQTT_USER, password: process.env.MQTT_PASSWORD})
 
- 
 client.on('connect', function (connack) {
   console.log('Connected to MQTT')
   client.subscribe('warema/#')
 })
 
 client.on('error', function (error) {
-  console.log('MQTT Error')
-  console.log(error.toString())
+  console.log('MQTT Error: ' + error.toString())
 })
 
 client.on('message', function (topic, message) {
-  // message is Buffer
   console.log(topic + ':' + message.toString())
   var device = parseInt(topic.split('/')[1])
   switch (message.toString()) {
@@ -88,26 +77,10 @@ client.on('message', function (topic, message) {
     }
 })
 
-var stickUsb = new WmsVbStickUsb( settingsPar.wmsSerialPort, 
+var stickUsb = new warema(settingsPar.wmsSerialPort,
   settingsPar.wmsChannel,
   settingsPar.wmsPanid,
   settingsPar.wmsKey,
   {},
-  callback);
-
-// setTimeout(function() { 
-//     stickUsb.vnBlindSetPosition( "Living room 1", 0, -100 ) 
-//   }, 
-// 2000 );
-
-// fs.access(path, fs.F_OK, (err) => {
-//   if (err) {
-//     console.error(err)
-//     return
-//   }
-
-//   lineReader.eachLine(path, function(line) {
-//     console.log(line);
-//     stickUsb.vnBlindAdd(line, line);
-//   });
-// })
+  callback
+);
